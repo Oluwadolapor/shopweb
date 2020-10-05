f97d152df9c83956dc8a9e29ea09a9203346fb3f
Onifade Dolapo <hardolipey@gmail.com>
2020-10-05 10:06:47 +0100

    Updates

---
+++ precache-manifest.1c08194390b5a232a7be17663076da8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d28bc6f5cae9c14a3dc078f2dd441d2",
+    "url": "/shopweb/index.html"
+  },
+  {
+    "revision": "e9011720fc456d7c58de",
+    "url": "/shopweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c86847a56569e5c674b9",
+    "url": "/shopweb/static/js/2.421e4138.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopweb/static/js/2.421e4138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9011720fc456d7c58de",
+    "url": "/shopweb/static/js/main.b21b48c5.chunk.js"
+  },
+  {
+    "revision": "6c87bc1c3266aeb5d61c",
+    "url": "/shopweb/static/js/runtime-main.d5200410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
